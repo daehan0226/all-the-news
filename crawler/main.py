1db783afb4a2e3e39c520852e03cc1d14cb012cb
daehan <dh16931@gmail.com>
2020-05-24 22:35:46 +0900

    chore: add chrome options

--- crawler/main.py
+++ crawler/main.py
@@ -5,7 +5,7 @@ import time
 import sys
 import simplejson
 from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 
 from libs.elasticsearch_wrapper import ElasticsearchWrapper
@@ -14,6 +14,33 @@ from crawlers.crawler_cnn import Crawler_cnn
 
 json_config = open("./config/config.json").read()
 config = simplejson.loads(json_config)
+
+# chrome과 함께 실행할 옵션 객체 생성
+options = Options()
+
+# chrome 브라우저 실행 환경 셋팅
+prefs = {
+    'profile.default_content_setting_values.notifications': 2,
+    'profile.default_content_setting_values.popups': 2,
+    'profile.default_content_settings.state.flash': 0,
+    'profile.managed_default_content_settings.images': 2,
+    'download.prompt_for_download': False
+}
+
+options.add_experimental_option("prefs", prefs)
+# options.binary_location = '/opt/google/chrome/google-chrome'
+options.add_argument('--disable-gpu')
+options.add_argument('--disable-infobars')
+options.add_argument('--ignore-certificate-errors-spki-list')
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--ignore-ssl-errors')
+options.add_argument('--disable-popup-blocking')
+# options.add_argument('--kiosk')
+options.add_argument('--start-maximized')
+# options.add_argument('--user-data-dir=' + CHROME_PROFILE)
+options.add_argument(
+    "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36")
 
 class Crawler:
     def __init__(self):
@@ -25,7 +52,7 @@ class Crawler:
 
     def run(self):
         CHROME_PATH = self.config["driver_path"]
-        self.driver = webdriver.Chrome(executable_path=CHROME_PATH)
+        self.driver = webdriver.Chrome(executable_path=CHROME_PATH, chrome_options=options)
         self.wait = WebDriverWait(self.driver, 15)
         self.es = ElasticsearchWrapper(self.config["es_host"], self.config["es_port"])
 
@@ -34,12 +61,12 @@ class Crawler:
             self.logging.info("parsing started for this site, " + site[0])
 
             if site[0] == "bbc":
-                cralwer = Crawler_bbc(self.driver, self.wait, self.logging, self.es, site[1])
+                cralwer = Crawler_bbc(self.driver, self.wait, self.logging, self.es, site)
                 cralwer.parse()
                 end_time = time.time()
 
             if site[0] == "cnn":
-                cralwer = Crawler_cnn(self.driver, self.wait, self.logging, self.es, site[1])
+                cralwer = Crawler_cnn(self.driver, self.wait, self.logging, self.es, site)
                 cralwer.parse()
                 end_time = time.time()
 
